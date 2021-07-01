ba48c30e1f74c6ddf258ff83af5832291ec18e31
cca <cca@163.com>
2021-07-01 22:43:43 +0800

    add header to list

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Header from "./components/Header"
+import List from "./components/List"
+import Footer from "./components/Footer"
+import "./App.css"
+
+export default class App extends Component {
+    state={todos:[
+        {id:'001',name:"吃饭",done:true},
+        {id:'002',name:"睡觉",done:true},
+        {id:'003',name:"打代码",done:false},
+        {id:'004',name:"逛街",done:true}
+    ]}
+    addTodo = (todoObj) => {
+        //获取原todos
+        const {todos}=this.state
+        //追加一个todo
+        const newTodos=[todoObj,...todos]
+        //更新状态
+        this.setState({todos:newTodos})
+    }
+    
+    render() {
+        const {todos}=this.state
+        return (
+            <div>
+                <div className="todo-container">
+                    <div className="todo-wrap">
+                        <Header addTodo={this.addTodo}/>
+                        <List todos={todos}/>
+                        <Footer/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
