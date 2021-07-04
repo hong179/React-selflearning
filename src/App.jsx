bffb0a3f13125d82fabcbb879eed0b73b9cc1f71
cca <cca@163.com>
2021-07-04 22:40:05 +0800

    update the checkbox status

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,18 @@ export default class App extends Component {
         //更新状态
         this.setState({todos:newTodos})
     }
-    
+    //用于更细一个todo对象
+    updateTodo=(id,done)=>{
+        //获取状态中的todos
+        const {todos}=this.state
+        //匹配处理数据
+        const newTodos=todos.map((todoObj)=>{
+           if (todoObj.id===id) return {...todoObj,done}
+           else return todoObj
+
+        })
+        this.setState({todos:newTodos})
+    }
     render() {
         const {todos}=this.state
         return (
@@ -27,7 +38,7 @@ export default class App extends Component {
                 <div className="todo-container">
                     <div className="todo-wrap">
                         <Header addTodo={this.addTodo}/>
-                        <List todos={todos}/>
+                        <List todos={todos} updateTodo={this.updateTodo}/>
                         <Footer/>
                     </div>
                 </div>
--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import "./index.css"
 
 export default class Item extends Component {
-    state = {mouse:false}
+    state = {mouse:false} //表示鼠标移入移出
 
     handleMouse=(flag)=>{
         return ()=>{
@@ -10,13 +10,21 @@ export default class Item extends Component {
         }
     }
 
+    handleCheck=(id)=>{
+        return (event)=>{
+            console.log(id,event.target.checked)
+
+        }
+
+    }
+
     render() {
-        const {name,done}=this.props
+        const {id,name,done}=this.props
         const {mouse} = this.state
         return (
             <li style={{backgroundColor: mouse ? '#ddd':'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} >
                 <label>
-                    <input type="checkbox" defaultChecked={done}/>
+                    <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)}/>
                     <span>{name}</span>
                 </label>
                 <button className="btn btn-danger" style={{display: mouse ? 'block' :'none'}}>删除</button>
